23e7d06f9e603f7e42bae709d7ab4e6d3dd798ee
Álvaro Alves <alvaro.a.alves@gmail.com>
2018-02-28 16:26:09 -0300

    Layout da página de Quiz quase finalizado

--- App/View/TesteView.js
+++ App/View/TesteView.js
@@ -11,50 +11,39 @@ class TesteView{
          let containerTeste = $(".teste-changes");
          if(numPergunta == 0){
              containerTeste.innerHTML = `
-             <h3>2 - Segunda pergunta</h3>
+             <h3><span>2</span><br>Pra Onde seria a sua viagem dos sonhos?</h3>
              <ul class="ul-perguntas">
-                 <li class="pergunta-1">Banana</li>
-                 <li class="pergunta-2">Uva</li>
-                 <li class="pergunta-3">Maçã</li>
+                 <li class="pergunta-1 li--imagem1--segunda li--transition">Nova York</li>
+                 <li class="pergunta-2 li--imagem2--segunda li--transition">Paris</li>
+                 <li class="pergunta-3 li--imagem3--segunda li--transition">Londres</li>
              </ul>
              `;
          }
 
          if(numPergunta == 1){
              containerTeste.innerHTML = `
-             <h3>3 - Terceira pergunta</h3>
+             <h3><span>3</span><br>Qual produto você não vive sem?</h3>
              <ul class="ul-perguntas">
-                 <li class="pergunta-1">Pêssego</li>
-                 <li class="pergunta-2">Maracujá</li>
-                 <li class="pergunta-3">Melancia</li>
+                 <li class="pergunta-1 li--imagem1--terceira li--transition">Delineador</li>
+                 <li class="pergunta-2 li--imagem2--terceira li--transition">Batom</li>
+                 <li class="pergunta-3 li--imagem3--terceira li--transition">Rímel</li>
              </ul>
              `;
          }
 
          if(numPergunta == 2){
              containerTeste.innerHTML = `
-             <h3>4 - Quarta pergunta</h3>
+             <h3 style="font-size:1.4em"><span>4</span><br>Qual é o seu programa favorito para o final de semana?</h3>
              <ul class="ul-perguntas">
-                 <li class="pergunta-1">Abacate</li>
-                 <li class="pergunta-2">Mamão</li>
-                 <li class="pergunta-3">Tomate</li>
+                 <li class="pergunta-1 li--imagem1--quarta li--transition">Show</li>
+                 <li class="pergunta-2 li--imagem2--quarta li--transition">Cinema</li>
+                 <li class="pergunta-3 li--imagem3--quarta li--transition">Balada</li>
              </ul>
              `;
          }
 
          if(numPergunta == 3){
-             containerTeste.innerHTML = `
-             <h3>5 - Quinta pergunta</h3>
-             <ul class="ul-perguntas">
-                 <li class="pergunta-1">Laranja</li>
-                 <li class="pergunta-2">Tamarindo</li>
-                 <li class="pergunta-3">Jaboticaba</li>
-             </ul>
-             `;
-         }
-
-         if(numPergunta == 4){
-             Resultado.calcula();
+            Resultado.calcula();
          }
 
          atribuiEvento();
